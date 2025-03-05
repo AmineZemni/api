08c314d006895984b5e399e498756513a395d3c2
Mzem <malek.zemni@octo.com>
2025-03-05 14:42:11 +0100

    feat: test package done

--- app/application/commands/calculate_lkd_bloc_command_handler.py
+++ app/application/commands/calculate_lkd_bloc_command_handler.py
@@ -11,18 +11,18 @@ class CalculateLKDBlocCommandHandler(Command):
     def execute(self) -> str:
         calculationId = "test"
 
-        # TODO : read CSV and call calculate_lkd_bloc from package
+        # TODO : read CSV from API input not from filesystem
 
-        aoc_step_path = "test_inputs/aoc_step.csv"
-        calc_process_path = "test_inputs/calc_process.csv"
-        cf_items_path = "test_inputs/cf_items.csv"
-        discount_types_path = "test_inputs/discount_types.csv"
-        reporting_process_path = "test_inputs/reporting_process.csv"
-        run_types_path = "test_inputs/run_types.csv"
-        timeframe_path = "test_inputs/timeframe.csv"
-        uao_path = "test_inputs/uao.csv"
-        lrc_input_proj_path = "test_inputs/lrc_input_proj.csv"
-        monthly_yield_curves_path = "test_inputs/monthly_yield_curves.csv"
+        aoc_step_path = "app/test_data/inputs/aoc_step.csv"
+        calc_process_path = "app/test_data/inputs/calc_process.csv"
+        cf_items_path = "app/test_data/inputs/cf_items.csv"
+        discount_types_path = "app/test_data/inputs/discount_types.csv"
+        reporting_process_path = "app/test_data/inputs/reporting_process.csv"
+        run_types_path = "app/test_data/inputs/run_types.csv"
+        timeframe_path = "app/test_data/inputs/timeframe.csv"
+        uao_path = "app/test_data/inputs/uao.csv"
+        lrc_input_proj_path = "app/test_data/inputs/lrc_input_proj.csv"
+        monthly_yield_curves_path = "app/test_data/inputs/monthly_yield_curves.csv"
 
         aoc_step_df = pd.read_csv(aoc_step_path)
         calc_process_df = pd.read_csv(
@@ -72,6 +72,13 @@ class CalculateLKDBlocCommandHandler(Command):
             coverage_indicators_df,
             monthly_yield_curves_df,
             cf_proj_df,
+        )
+
+        ic_lrc_prm_lkd_m_aoc_df.to_csv(
+            "app/test_data/outputs/ic_lrc_prm_lkd_m_aoc.csv",
+            sep=";",
+            decimal=",",
+            index=False,
         )
 
         return calculationId
