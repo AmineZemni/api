b23ce0eca06b29101383eaa5f6794d308a71b4c3
Mohamed Achref OUESLATI <138464763+maoueslaty@users.noreply.github.com>
2025-03-05 17:05:52 +0100

    feature : dynamic input from user  (#4)
    * feat(calc) : add dynamic input csv
    
    * review
    
    ---------
    
    Co-authored-by: Mzem <malek.zemni@octo.com>

--- app/infrastructure/controllers/calculations_controller.py
+++ app/infrastructure/controllers/calculations_controller.py
@@ -1,24 +1,54 @@
-from app.infrastructure.controllers.schemas import (
-    CalculationIdResponse,
+from app.infrastructure.controllers.request_schemas import (
+    CalculationLKDRequest,
     CalculationSampleRequest,
+)
+from app.infrastructure.controllers.response_schemas import (
     CalculationSampleResponse,
+    CalculationIdResponse,
 )
 from app.application.commands.add_values_command_handler import addValuesCommandHandler
 from app.application.commands.calculate_lkd_bloc_command_handler import (
     calculateLKDBlocCommandHandler,
 )
-from fastapi import APIRouter
+
+from fastapi import APIRouter, UploadFile, File, HTTPException
 
 calculations_router = APIRouter()
 
 
 @calculations_router.post("/sample", response_model=CalculationSampleResponse)
 def postCalculation(calculation: CalculationSampleRequest):
-    result = addValuesCommandHandler.execute(calculation.value1, calculation.value2)
+    result = addValuesCommandHandler.execute(calculation)
     return CalculationSampleResponse(result=result)
 
 
 @calculations_router.post("/lkd-bloc", response_model=CalculationIdResponse)
-def postCalculation():
-    id = calculateLKDBlocCommandHandler.execute()
-    return CalculationIdResponse(id=id)
+async def calculate_lkd(
+    aoc_step_file: UploadFile = File(...),
+    calc_process_file: UploadFile = File(...),
+    cf_items_file: UploadFile = File(...),
+    discount_types_file: UploadFile = File(...),
+    reporting_process_file: UploadFile = File(...),
+    run_types_file: UploadFile = File(...),
+    timeframe_file: UploadFile = File(...),
+    uao_file: UploadFile = File(...),
+    lrc_input_proj_file: UploadFile = File(...),
+    monthly_yield_curves_file: UploadFile = File(...),
+):
+    """Endpoint to calculate LKD using uploaded CSV files."""
+
+    calculation_files = CalculationLKDRequest(
+        aoc_step_file=aoc_step_file,
+        calc_process_file=calc_process_file,
+        cf_items_file=cf_items_file,
+        discount_types_file=discount_types_file,
+        reporting_process_file=reporting_process_file,
+        run_types_file=run_types_file,
+        timeframe_file=timeframe_file,
+        uao_file=uao_file,
+        lrc_input_proj_file=lrc_input_proj_file,
+        monthly_yield_curves_file=monthly_yield_curves_file,
+    )
+
+    calculation_id = await calculateLKDBlocCommandHandler.execute(calculation_files)
+    return CalculationIdResponse(id=calculation_id)
--- app/infrastructure/controllers/response_schemas.py
+++ app/infrastructure/controllers/response_schemas.py
@@ -1,11 +1,6 @@
 from pydantic import BaseModel
 
 
-class CalculationSampleRequest(BaseModel):
-    value1: float
-    value2: float
-
-
 class CalculationSampleResponse(BaseModel):
     result: float
 
